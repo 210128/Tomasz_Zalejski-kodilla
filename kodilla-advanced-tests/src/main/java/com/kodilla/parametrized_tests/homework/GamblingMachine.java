71c52ecb96343563be985a5dc7d92485342dc114
Tomasz Zalejski <210128@wp.pl>
2020-02-24 14:59:15 +0100

    homework 7.2

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/GamblingMachine.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/GamblingMachine.java
@@ -1,9 +1,8 @@
 package com.kodilla.parametrized_tests.homework;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.Random;
+import java.util.Set;
 
 public class GamblingMachine {
     public int howManyWins(Set<Integer> userNumbers) throws InvalidNumbersException {
@@ -17,30 +16,22 @@ public class GamblingMachine {
         }
         return count;
     }
+
     private void validateNumbers(Set<Integer> numbers) throws InvalidNumbersException {
-        if(numbers.size() != 6){
+        if (numbers.size() != 6) {
             throw new InvalidNumbersException();
         }
-        if ((numbers.stream().anyMatch(number -> number <1))) {
+        if ((numbers.stream().anyMatch(number -> number < 1))) {
             throw new InvalidNumbersException();
         }
     }
+
     private Set<Integer> generateComputerNumbers() {
         Set<Integer> numbers = new HashSet<>();
         Random generator = new Random();
-        while (numbers.size() <6){
-            numbers.add(generator.nextInt(49 )+1);
+        while (numbers.size() < 6) {
+            numbers.add(generator.nextInt(49) + 1);
         }
         return numbers;
-    }
-
-    public static void main(String[] args) throws InvalidNumbersException {
-        GamblingMachine gamblingMachine = new GamblingMachine();
-       Integer[] zz={1,2,3,4,5,49};
-       Set<Integer> userNumbers = new HashSet<>(Arrays.asList(zz));
-        System.out.println(userNumbers.size());
-        System.out.println(gamblingMachine.generateComputerNumbers());
-        System.out.println(gamblingMachine.howManyWins(userNumbers));
-
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -12,30 +12,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GamblingMachineTest {
     private GamblingMachine gamble = new GamblingMachine();
+
     @ParameterizedTest
     @CsvFileSource(resources = "/intUserNumbers.csv", numLinesToSkip = 1)
-    public void testHowManyWins_WithException(int a, int b, int c, int d, int e, int f)throws InvalidNumbersException {
-        Integer[] data = {a,b,c,d,e,f};
+    public void testHowManyWins_WithException(int a, int b, int c, int d, int e, int f) throws InvalidNumbersException {
+        Integer[] data = {a, b, c, d, e, f};
         Set<Integer> numbers = new HashSet<>(Arrays.asList(data));
-assertThrows(InvalidNumbersException.class, ()->gamble.howManyWins(numbers));
+        assertThrows(InvalidNumbersException.class, () -> gamble.howManyWins(numbers));
     }
+
     @ParameterizedTest
     @CsvFileSource(resources = "/intUserNumbers.csv")
-    public void testHowManyWins_WithExceptionWhenNotEnoughNumbersAreGiven(int a, int b, int c, int d, int e)throws InvalidNumbersException {
-        Integer[] data = {a,b,c,d,e};
+    public void testHowManyWins_WithExceptionWhenNotEnoughNumbersAreGiven(int a, int b, int c, int d, int e) throws InvalidNumbersException {
+        Integer[] data = {a, b, c, d, e};
         Set<Integer> numbers = new HashSet<>(Arrays.asList(data));
-        assertThrows(InvalidNumbersException.class, ()->gamble.howManyWins(numbers));
+        assertThrows(InvalidNumbersException.class, () -> gamble.howManyWins(numbers));
     }
+
     @ParameterizedTest
     @CsvFileSource(resources = "/intUserNumbers1.csv")
-    public void testHowManyWins_WithExceptionWhenToManyNumbersAreGiven(int a, int b, int c, int d, int e,int f, int g)throws InvalidNumbersException {
-        Integer[] data = {a,b,c,d,e,f,g};
+    public void testHowManyWins_WithExceptionWhenToManyNumbersAreGiven(int a, int b, int c, int d, int e, int f, int g) throws InvalidNumbersException {
+        Integer[] data = {a, b, c, d, e, f, g};
         Set<Integer> numbers = new HashSet<>(Arrays.asList(data));
-        assertThrows(InvalidNumbersException.class, ()->gamble.howManyWins(numbers));
+        assertThrows(InvalidNumbersException.class, () -> gamble.howManyWins(numbers));
     }
+
     @ParameterizedTest
     @CsvFileSource(resources = "/properIntUserNumbers.csv")
-    public void shouldReturnTrueWhenWeEnter6NumbersInRange1To49HowManyWins(int a, int b, int c, int d, int e,int f)throws InvalidNumbersException {
+    public void shouldReturnTrueWhenWeEnter6NumbersInRange1To49HowManyWins(int a, int b, int c, int d, int e, int f) throws InvalidNumbersException {
         Integer[] data = {a, b, c, d, e, f};
         Set<Integer> numbers = new HashSet<>(Arrays.asList(data));
         System.out.println(gamble.howManyWins(numbers));
