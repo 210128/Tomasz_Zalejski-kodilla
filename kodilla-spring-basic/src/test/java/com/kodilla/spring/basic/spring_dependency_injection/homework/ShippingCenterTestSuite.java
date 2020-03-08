ad09d30bcbfb755d8031d2152d900d2d8f50c884
Tomasz Zalejski <210128@wp.pl>
2020-03-08 20:22:12 +0100

    homework 9.3

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenterTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ShippingCenterTestSuite {
+    @Test
+    public void shouldSendSuccessMessage() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_dependency_injection.homework");
+        NotificationService bean = context.getBean(NotificationService.class);
+        String message = bean.success(" Wiejska 4");
+        Assertions.assertNotNull(message);
+    }
+
+    @Test
+    public void shouldSendFailMessage() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_dependency_injection.homework");
+        NotificationService bean = context.getBean(NotificationService.class);
+        String message = bean.fail(" Wiejska 4");
+        Assertions.assertNotNull(message);
+    }
+
+    @Test
+    public void shouldSendMessageAboutSuccessDelivery() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_dependency_injection.homework");
+        DeliveryService bean = context.getBean(DeliveryService.class);
+        boolean message = bean.deliverPackage("Wiejska 4", 25);
+        Assertions.assertTrue(message);
+    }
+
+    @Test
+    public void shouldSendMessageAboutFailDelivery() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_dependency_injection.homework");
+        DeliveryService bean = context.getBean(DeliveryService.class);
+        boolean message = bean.deliverPackage("Wiejska 4", 50.1);
+        Assertions.assertFalse(message);
+    }
+
+}
