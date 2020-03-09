39db26bed508b8598c37287a5735ce0074b96b51
Tomasz Zalejski <210128@wp.pl>
2020-03-09 18:31:34 +0100

    homework 9.4 not finished

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.Random;
+
+@Configuration
+public class CarFactory {
+
+    @Bean
+    public Car randomCar() {
+        Car car;
+        Random generator = new Random();
+        int season = generator.nextInt(12) + 1;
+        int hours = generator.nextInt(25);
+        if (season > 0 && season < 3 || season == 12) {
+            car = new SUV();
+        } else if (season > 2 && season < 6) {
+            car = new Sedan();
+        } else if (season > 5 && season < 9) {
+            car = new Cabrio();
+        } else {
+            car = new Sedan();
+        }
+        return car;
+    }
+}
