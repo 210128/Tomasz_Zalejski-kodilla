012ffe981abb5b9eba5144384dec593b246c0718
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-04-04 08:37:53 +0200

    13.3 Facebook tests remarks

--- kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestApp {
+    /*TODO: It is better to run such tests with JUnit, so that statistics will be displayed - numnber of tests passed and failed*/
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -15,5 +16,10 @@ public class FacebookTestApp {
         WebElement yearCombo = driver.findElement(By.xpath("//*[@id=\"birthday_wrapper\"]/div/span/span/select[3]"));
         Select yearSelect = new Select(yearCombo);
         yearSelect.selectByIndex(5);
+
+        /*  TODO: It's better to select items by value. When run next year, selectByIndex will pick a different date*/
+        yearSelect.selectByValue("2011");
+
+        //driver.close();
     }
 }
