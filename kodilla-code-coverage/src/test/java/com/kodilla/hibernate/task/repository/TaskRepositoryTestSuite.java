0775f0deb8ee2154645b573e7ca763c0e538a6e6
Tomasz Zalejski <210128@wp.pl>
2020-04-22 22:31:20 +0200

    Exercise 15.3

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/task/repository/TaskRepositoryTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.hibernate.task.repository;
+
+import com.kodilla.hibernate.task.Task;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskRepositoryTestSuite {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    public void testTaskRepositorySave() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+
+        //When
+        taskRepository.save(task);
+
+        //Then
+        int id = task.getId();
+        Optional<Task> readTask = taskRepository.findById(id);
+        Assert.assertTrue(readTask.isPresent());
+
+        //CleanUp
+        taskRepository.deleteById(id);
+    }
+
+    @Test
+    public void testTaskRepositoryFindByDuration() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+        taskRepository.save(task);
+        int duration = task.getDuration();
+        //When
+        List<Task> readTask = taskRepository.findByDuration(duration);
+        //Then
+        Assert.assertEquals(1, readTask.size());
+        //CleanUp
+        int id = readTask.get(0).getId();
+        taskRepository.deleteById(id);
+    }
+
+
+}
---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListRepositoryTestSuite {
+    @Autowired
+    private TaskListRepository taskListRepository;
+
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("Hibernate", "Test find by ListName");
+        taskListRepository.save(taskList);
+        String listName = taskList.getListName();
+        //When
+        List<TaskList> readTaskList = taskListRepository.findByListName(listName);
+        //Then
+        Assert.assertEquals(1, readTaskList.size());
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListRepository.deleteById(id);
+    }
+
+}
